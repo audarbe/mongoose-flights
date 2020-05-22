2f11ad8742a0428ee9b5596c7fc45b5242d5011c
Alexei Udarbe <alexeiudarbe@AU-MacBook-Bro.local>
2020-05-22 13:13:33 -0700

    Updated sort & color for elapsed flights

--- controllers/flights.js
+++ controllers/flights.js
@@ -8,7 +8,11 @@ module.exports = {
 
 function index(req, res) {
     Flight.find({}, function(err, flights) {
-        res.render('flights/index', {flights});
+        res.render('flights/index', {
+            flights,
+        });
+    }).sort({
+        departs: 'ascending'
     });
 };
 
@@ -16,13 +20,14 @@ function newFlight(req, res) {
     const newFlight = new Flight();
     const dt = newFlight.departs;
     const departsDate = `${dt.getFullYear()}-${dt.getMonth().toString().padStart(2, '0')}-${dt.getDate()}T${dt.getHours().toString().padStart(2, '0')}:${dt.getMinutes().toString().padStart(2, '0')}`;
-    res.render('flights/new', {departsDate});
+    res.render('flights/new',
+     {departsDate});
 };
 
 function create (req, res) {
     const flight = new Flight(req.body);
     flight.save(function() {
-        console.log(flight);
+        console.log(flight); // remove this before submit
         res.redirect('/flights');
     });
 };
@@ -30,7 +35,6 @@ function create (req, res) {
 
 /*
 Code these additional User Stories:
-AAU, I want to view the list of flights by departure date in ascending order.
 AAU, I want the flights in the list to be displayed using red text if the flight's departure date has passed.
 Style the index and new views.
 */
